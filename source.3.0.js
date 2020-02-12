3ff16401da96e817db4865cdd06e8065af1bde1d
an3z4m <kerkacha.nabil@gmail.com>
2020-02-12 15:42:24 +0100

    first commit

---
+++ source.3.0.js
@@ -0,0 +1,358 @@
+
+Sourah = [
+    ["Al-fatiha",[],"الفاتحة"],
+    ["Al-bakara",[16,25,33,41,53,59,66,75,85,91,100,105,113,123,132,141,147,157,165,176,184,188,196,202,212,218,226,232,235,242,247,252,257,262,266,271,279,282,0.75],"البقرة"],
+    ["Ali-3imran",[0.25,4,14,22,32,41,51,63,74,81,91,102,112,120,132,143,152,159,170,180,185,195,0.33],"آل عمران"],
+    ["Al-nissa",[0.67,5,10,12,19,23,28,35,45,57,64,73,78,86,92,99,104,113,123,129,139,147,157,165,173,0.5],"النساء"],
+    ["Al-Maida",[0.5,2,5,11,16,22,32,40,44,48,57,66,72,81,89,96,105,110,120],"المائدة"],
+    ["Al-An3am",[12,25,35,47,58,69,79,90,94,102,110,119,126,134,139,144,150,157,0.67],"الأنعام"],
+    ["Al-A3raf",[0.33,4,20,31,38,46,56,68,76,87,99,116,131,141,148,155,162,170,178,187,198,0.67],"الأعراف"],
+    ["Al-anfal",[0.33,4,14,21,30,40,47,59,70,0.5],"الأنفال"],
+    ["Al-tawba",[0.5,4,11,18,27,33,39,45,54,60,69,74,84,92,100,110,116,122,0.75],"التوبة"],
+    ["Younes",[0.25,2,10,18,25,36,48,60,70,82,92,103,0.5],"يونس"],
+    ["Houd",[0.5,5,14,23,31,40,52,61,72,83,92,104,117,0.33],"هود"],
+    ["Youssef",[0.67,9,22,32,41,52,65,76,87,100,110],"يوسف"],
+    ["Al-ra3d",[4,13,18,29,34,43],"الرعد"],
+    ["Ibrahim",[9,18,27,38,52],"إبراهيم"],
+    ["Al-hidjr",[21,48,77,99],"الحجر"],
+    ["Al-nahl",[14,29,37,50,62,70,78,89,97,110,119,128],"النحل"],
+    ["Al-israa",[10,22,35,49,60,69,84,98,111],"الإسراء"],
+    ["Al-kahf",[16,22,29,41,49,58,74,86,101,0.75],"الكهف"],
+    ["Mariam",[0.25,6,21,40,58,74,98],"مريم"],
+    ["Taha",[35,54,71,82,96,110,125,135],"طه"],
+    ["Al-anbiaa",[14,29,41,50,72,86,100,112],"الأنبياء"],
+    ["Al-hadj",[10,18,29,37,48,59,71,78],"الحج"],
+    ["Al-mouminoun",[23,38,56,74,92,114,0.25],"المومنون"],
+    ["Al-nour",[0.75,10,20,29,34,42,52,59,62,0.25],"النور"],
+    ["Al-fourqan",[0.75,9,20,34,44,60,77],"الفرقان"],
+    ["Al-cho3araa",[24,49,81,110,145,180,209,0.67],"الشعراء"],
+    ["Al-naml",[0.33,5,16,26,40,55,65,81,93],"النمل"],
+    ["Al-qassas",[11,21,28,38,50,62,75,82,0.5],"القصص"],
+    ["Al-ankabout",[0.5,7,16,25,35,45,59,0.67],"العنكبوت"],
+    ["Al-roum",[0.33,8,21,29,40,53,0.5],"الروم"],
+    ["Loqman",[0.5,11,21,32,0.33],"لقمان"],
+    ["Al-sadjda",[0.67,10,22,0.5],"السجدة"],
+    ["Al-ahzab",[0.5,5,12,17,24,30,37,48,52,59,73],"الأحزاب"],
+    ["Sabaa",[9,17,23,36,45,0.5],"سبأ"],
+    ["Fatir",[0.5,7,14,30,40,0.83],"فاطر"],
+    ["Yacin",[0.17,6,27,45,59,79,0.25],"يس"],
+    ["Safat",[0.75,21,50,82,113,144,182],"الصافات"],
+    ["Sad",[20,30,51,83,0.17],"ص"],
+    ["Al-zoumar",[0.83,7,20,31,41,53,66,75],"الزمر"],
+    ["Ghafir",[10,20,29,40,52,65,78,0.5],"غافر"],
+    ["Foussilet",[0.5,8,16,24,37,46,0.83],"فصلت"],
+    ["Al-choura",[0.17,4,12,19,26,41,50,0.5],"الشورى"],
+    ["Al-zoukhrouf",[0.5,10,23,42,62,84,0.25],"الزخرف"],
+    ["Al-doukhan",[0.75,24,59],"الدخان"],
+    ["Al-djathia",[13,24,37],"الجاثية"],
+    ["Al-ahkaf",[14,20,32,0.33],"الأحقاف"],
+    ["Mohammed(s)",[0.67,9,19,32,0.5],"محمد"],
+    ["Al-fath",[0.5,7,17,26,29],"الفتح"],
+    ["Al-houdjourat",[8,13,0.5],"الحجرات"],
+    ["Qaf",[0.5,8,26,45],"ق"],
+    ["Al-thariat",[30,55,0.25],"الذاريات"],
+    ["Al-tour",[0.75,23,43,0.33],"الطور"],
+    ["Al-nadjm",[0.67,25,46,0.5],"النجم"],
+    ["Al-qamar",[0.5,9,36,55],"القمر"],
+    ["Al-rahman",[40,78],"الرحمان"],
+    ["Al-waqia",[48,74,0.5],"الواقعة"],
+    ["Al-hadid",[0.5,6,15,21,29],"الحديد"],
+    ["Al-moudjadala",[7,13,0.75],"المجادلة"],
+    ["Al-hachr",[0.25,2,10,24],"الحشر"],
+    ["Al-moumtahana",[6,13],"الممتحنة"],
+    ["Al-saf",[14],"الصف"],
+    ["Al-djoumou3a",[11],"الجمعة"],
+    ["Al-mounafiqoun",[8,0.33],"المنافقون"],
+    ["Al-taghaboun",[0.67,6,18],"التغابن"],
+    ["Al-talaq",[6,12],"الطلاق"],
+    ["Al-tahrim",[7,12],"التحريم"],
+    ["Al-mulk",[18],"الملك"],
+    ["Al-qalam",[18,47],"القلم"],
+    ["Al-haqa",[18,52],"الحاقة"],
+    ["Al-ma3areedj",[39],"المعارج"],
+    ["Nouh",[14,28],"نوح"],
+    ["Al-djen",[23],"الجن"],
+    ["Al-muzamil",[19],"المزمل"],
+    ["Al-mudather",[30,56],"المدثر"],
+    ["Al-qiama",[40],"القيامة"],
+    ["Al-insane",[18],"الإنسان"],
+    ["Al-moursalat",[15,50],"المرسلات"],
+    ["Al-nabaa",[40],"النبأ"],
+    ["Al-nazi3at",[41],"النازعات"],
+    ["3abassa",[42],"عبس"],
+    ["Al-takwir",[29],"التكوير"],
+    ["Al-infitar",[],"الإنفطار"],
+    ["Al-moutaffifine",[13],"المطففين"],
+    ["Al-inchiqaq",[15],"الإنشقاق"],
+    ["Al-bouroudj",[11],"البروج"],
+    ["Al-tarik",[17],"الطارق"],
+    ["Al-a3la",[],"الأعلى"],
+    ["Al-ghachia",[16],"الغاشية"],
+    ["Al-fadjr",[30],"الفجر"],
+    ["Al-balad",[],"البلد"],
+    ["Al-chams",[15],"الشمس"],
+    ["Al-lail",[],"الليل"],
+    ["Al-douha",[11],"الضحى"],
+    ["Al-charh",[],"الشرح"],
+    ["Al-teen",[],"التين"],
+    ["Al-3alaq",[],"العلق"],
+    ["Al-qadr",[5],"القدر"],
+    ["Al-bayena",[],"البينة"],
+    ["Al-zalzala",[],"الزلزلة"],
+    ["Al-3adiat",[8],"العاديات"],
+    ["Al-qari3a",[],"القارعة"],
+    ["Al-takathour",[],"التكوير"],
+    ["Al-3asr",[],"العصر"],
+    ["Al-houmaza",[],"الهمزة"],
+    ["Al-feel",[5],"الفيل"],
+    ["Kouraich",[],"قريش"],
+    ["Al-ma3oun",[],"الماعون"],
+    ["Al-kawthar",[],"الكوثر"],
+    ["Al-kaferoun",[],"الكافرون"],
+    ["Al-nasr",[],"النصر"],
+    ["Al-massad",[],"المسد"],
+    ["Al-ikhlass",[],"الإخلاص"],
+    ["Al-falaq",[],"الفلق"],
+    ["Al-nas",[6],"الناس"]
+];
+AyahNumBySourah=[7,286,200,176,120,165,206,75,129,109,123,111,43,52,99,128,111,110,98,135,112,78,118,64,77,227,93,88,69,60,34,30,73,54,45,83,182,88,75,85,54,53,89,59,37,35,38,29,18,45,60,49,62,55,78,96,29,22,24,13,14,11,11,18,12,12,30,52,52,44,28,28,20,56,40,31,50,40,46,42,29,19,36,25,22,17,19,26,30,20,15,21,11,8,8,19,5,8,8,11,11,8,3,9,5,4,7,3,6,3,5,4,5,6];
+ThmnBySourah=[];
+AvgBySourah=[];
+
+/*
+1------1
+5/6----0.83
+3/4----0.75
+2/3----0.67
+1/2----0.5
+1/3----0.33
+1/4----0.25
+1/6----0.17
+*/
+function getThmnBySourah(){
+	for(var i=0;i<Sourah.length;i++){
+		ThmnBySourah[i] = 0;
+		for(var j=0;j<Sourah[i][1].length;j++){
+			if(Sourah[i][1][j]>=1){
+				ThmnBySourah[i]++;
+			}else{
+				ThmnBySourah[i] += Sourah[i][1][j];
+			}
+		}
+			if(Sourah[i][1][0]<1){ ThmnBySourah[i]--; }
+	}
+}
+
+function getAvgBySourah(){
+	getThmnBySourah();
+	for(var i=0;i<Sourah.length;i++){
+		AvgBySourah[i] = ThmnBySourah[i]/AyahNumBySourah[i];
+	}
+}
+
+function findSourah(str){
+	console.log(str);
+    for(var i=0;i<Sourah.length;i++){
+        if(Sourah[i][2]==str){ return i; }
+    }
+    return -1;
+}
+function findSourahs(str){
+    var result=[];
+    for(var i=0;i<Sourah.length;i++){
+        if(Sourah[i][2].match(".*"+str+".*")){ result.push(i); }
+    }
+    return result;
+}
+
+function countThmnsBySourahName(beginSourah,beginAyah,endSourah,endAyah){
+    var begin   = verifyExistance(beginSourah,beginAyah);
+    var end     = verifyExistance(endSourah,endAyah);
+    if(begin>=0 && end>=0){  // none is equal to -1 so they are both true
+	return countThmnsBySourahNum(begin,beginAyah,end,endAyah);
+    }else{
+	console.log("ERROR : "+begin,end);	
+        return -1;
+    }
+}
+function countThmnsBySourahNum(beginSourah,beginAyah,endSourah,endAyah){
+	if(beginSourah==endSourah){  // in the same Sourah
+	    return IndexOfAyahInSourah(beginSourah,endAyah)-IndexOfAyahInSourah(beginSourah,beginAyah);
+	}
+	var sum = ThmnBySourah[beginSourah]-IndexOfAyahInSourah(beginSourah,beginAyah);
+if(beginSourah>endSourah){
+	for(var indexSourah = beginSourah-1;indexSourah>endSourah;indexSourah--){    // inc ---- ÕÚæÏÇ
+		sum += ThmnBySourah[indexSourah];
+	}
+}else{
+	for(var indexSourah = beginSourah+1;indexSourah<endSourah;indexSourah++){    // desc ---- äÒæáÇ
+		sum += ThmnBySourah[indexSourah];
+	}
+}
+	sum += IndexOfAyahInSourah(endSourah,endAyah);
+	return sum;  
+}
+function IndexOfAyahInSourah(sourahNum,Ayah){
+	var index = 0;
+	if(Sourah[sourahNum][1].length>0){
+//	SOLVING THE (<1 in the beginning) PROBLEM x=7-->[0.5,10,26]
+		if(Sourah[sourahNum][1][0]<1){
+			if(Ayah<Sourah[sourahNum][1][1]){ return (Ayah-1)/(Sourah[sourahNum][1][1]-1)*Sourah[sourahNum][1][0]; }
+			index++;   //index=1		
+		}
+		while(Ayah>Sourah[sourahNum][1][index] && index<Sourah[sourahNum][1].length){
+			index++;
+		}
+//	SOLVING THE (<1 in the beginning) PROBLEM x=7-->[10,26]
+		if(index==0){ return (Ayah-1)/(Sourah[sourahNum][1][0]-1); }
+	
+//	SOLVING THE (<1 in the end) PROBLEM   x=26-->[11,23,0.67]
+		if(index==Sourah[sourahNum][1].length){
+			index--;
+			index += (Ayah-Sourah[sourahNum][1][index-1])/(AyahNumBySourah[sourahNum]-Sourah[sourahNum][1][index-1])*Sourah[sourahNum][1][index];
+		}else{ // x=15-->[11,23,0.67]
+			index += (Ayah-Math.max(1,Sourah[sourahNum][1][index-1]))/(Sourah[sourahNum][1][index]-Math.max(1,Sourah[sourahNum][1][index-1]));	
+		}
+		if(Sourah[sourahNum][1][0]<1){ index += Sourah[sourahNum][1][0]-2; }
+	}
+	return index;
+}
+
+function getHizbsFromThmns(thmnNum){
+	under_thmn = findFraction(thmnNum-Math.floor(thmnNum));
+	if(under_thmn[0]*under_thmn[1]==1){ thmnNum++; under_thmn=[0,1]; }
+	hizb_num = Math.floor(thmnNum/8);
+	thmn_num = Math.floor(thmnNum) - (hizb_num*8);
+	return {'hizb':hizb_num,'thmn':thmn_num,'part':under_thmn};
+}
+
+function findFraction(num){
+    if(num<0.3){
+	return [0,1];
+    }else if(num>0.75){ //]0.875,1]
+        return [1,1];
+    }else{ //0.67
+        return [1,2];
+    }
+}
+function getString(result){
+	var str = "";
+	if(result.hizb>0){ 
+		if(result.hizb==1){
+			str += " حزب ";
+		}else if(result.hizb==2){
+			str += " حزبان ";
+		}else if(result.hizb<11){
+			str += result.hizb+" أحزاب ";
+		}else{
+			str += result.hizb+"حزبا ";
+		}
+		if(result.thmn>0 || result.part[1]!=1){ str += "و "; }else{ return str; }}
+	if(result.thmn>0){
+		if(result.thmn==1){
+			str += " ثمن ";
+		}else if(result.thmn==2){
+			str += " ثمنان ";
+		}else if(result.thmn<11){
+			str += result.thmn+" أثمان ";
+		}else{
+			str += result.thmn+" ثمنا ";
+		}
+		if(result.part[1]!=1){ str += "و "; }else{ return str; }}
+	if(result.part[1]!=1){	str += getStringFraction(result.part);}
+	 return str;
+}
+function getStringFraction(part){
+	if(part[0]==1){
+	 if(part[1]==2 ){ return "نصف ثمن"; }
+	 if(part[1]==3 ){ return "ثلث ثمن"; }
+	 if(part[1]==4 ){ return "ربع ثمن"; }
+	 if(part[1]==5 ){ return "خمس ثمن"; }
+	 if(part[1]==6 ){ return "سدس ثمن"; }
+	}
+	if(part[0]==2){  return "ثلثا ثمن"; }
+	if(part[0]==3){  return "ثلاث أرباع الثمن"; }
+}
+
+function showResult(){
+	var allChecked=true;
+	for(var i=0;i<input.length;i++){ if(input[i]==-1){ document.getElementById(i).focus(); return -1; }; }
+	result = countThmnsBySourahNum(input[0],input[1],input[2],input[3]).toFixed(2);
+   if(result != -1){
+	document.getElementById(5).value=getString(getHizbsFromThmns(result));
+	document.getElementById(4).value=result+" ثمنا ";
+   }
+}
+var fastSourahCheck = function(){
+	var str = this.value;
+	var found = -1;
+	if(str.match("[0-9]{1,3}")){
+		if(str>0 && str<=114){ found=str-1; }
+	}else{
+	var result=[];
+    	for(var i=0;i<Sourah.length;i++){
+        	if(Sourah[i][2].match(".*"+str+".*")){ result.push(i); }
+    	}
+    	if (result.length==1){ found=result[0]; };
+	}
+	if(found!=-1){
+		 this.className='correct'; this.value=Sourah[found][2];
+	 }else{
+	 	this.className='error';
+	}
+	input[this.id]=found;
+	showResult();
+}
+
+var fastAyahCheck = function(){
+	var selectedSourah = input[this.id-1];
+	var str = this.value;
+	if(str>0 && str<=AyahNumBySourah[selectedSourah]){ 
+		this.className='correct'; input[this.id]=str; 
+	}else{
+		this.className='error'; input[this.id]=-1;
+	}
+	showResult();
+}
+function checkScreenSize(){
+	fontSize=window.innerHeight/12;
+	document.getElementsByTagName('body')[0].style.fontSize=fontSize;
+	document.getElementsByTagName('body')[0].style.width=0.98*window.innerWidth;
+	console.log(window.innerWidth+'..'+window.innerHeight+'..'+fontSize);
+}
+var clickInput = function(){
+	if(focusIn == this.id){
+		this.value='';
+	}
+
+}
+var focusInput = function(){
+	focusIn = this.id;
+}
+
+function initialize(){
+	input = [-1,-1,-1,-1];
+	focusIn = 0;
+	document.getElementById(0).focus();
+for(var i=0;i<4;i++){
+	document.getElementById(i).value=title[i];
+	if(i%2==0){	
+		document.getElementById(i).onblur=fastSourahCheck;
+	}else{	document.getElementById(i).onblur=fastAyahCheck;}
+	document.getElementById(i).onfocus=focusInput;
+	document.getElementById(i).onclick=clickInput;
+}	
+	document.getElementById(4).value=title[4];
+	document.getElementById(5).value=title[5];
+	checkScreenSize();
+	window.onresize=checkScreenSize;
+}
+
+getThmnBySourah();
+
+var input = [];
+var data = ['beginSourah','beginAyah','endSourah','endAyah','by_thmn','total'];
+var title = ['من سورة','من الآية','إلى سورة','إلى الآية','عدد الأثمان','النتيجة المفصلة'];
+var focusIn;
+
+
